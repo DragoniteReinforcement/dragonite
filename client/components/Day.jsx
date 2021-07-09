984f1045caa3b7b804c3cb413d12d5eeebcbb38b
Will Ramirez <willramirez630@gail.com>
2021-07-08 21:09:11 -0400

    got more functionality on the frontend working

--- client/components/Day.jsx
+++ client/components/Day.jsx
@@ -1,20 +1,14 @@
 import React, { useEffect, useState } from 'react';
 
 const Day = (props) => (
-  // const [completed, setCompleted] = useState(false);
-
-  // const handleClick = (e) => {
-
-  // };
 
   <div className="dayBox">
     <p>Day: {props.day}</p>
-    <p>Date: {props.date}</p>
     <p>Task: {props.task}</p>
     <form>
       <label>
         Completed:
-        <input name="completed" type="checkbox" onClick={handleClick} />
+        <input name="completed" type="checkbox"/>
       </label>
     </form>
   </div>
--- client/components/JoinEventForm.jsx
+++ client/components/JoinEventForm.jsx
@@ -1,48 +1,67 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import { useRecoilValue, useRecoilState } from 'recoil';
 import atoms from '../atoms';
+import { Link, useHistory, BrowserRouter, Route} from 'react-router-dom';
+
 
 const JoinEventForm = () => {
-  const handleSubmit = (e) => {
-    e.preventDefault();
+  // const [joinedEvent, setJoinedEvent] = useState(false);
+  // // const history = useHistory();
+  
+  // const user = useRecoilValue(atoms.userInfo);
+  // const { username } = user;
+  // const [tasks, setTasks] = useRecoilState(atoms.userTasks);
+  // const [event, setEvent] = useRecoilState(atoms.events);
+  
 
-    const eventID = document.getElementById('eventID').value;
-    const user = useRecoilValue(atoms.userInfo);
-    const [tasks, setTasks] = useRecoilState(atom.userTasks);
-    const [event, setEvent] = useRecoilState(atom.events);
-    const { username } = user;
-    // writes tasks to userTasks Atom
-    fetch('/userJoinsEvent', {
-      method: 'POST', // or 'PUT'
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify(eventID, username),
-    })
-      .then((response) => response.json())
-      .then((arrOfTasks) => setTasks(arrOfTasks))
-      .catch((err) => console.log('join event err ', err));
+  // const handleSubmit = (e) => {
+  //   e.preventDefault();
+  //   const eventID = document.getElementById('eventID').value;
+    
+    // // // // writes tasks to userTasks Atom
+    // fetch('/userJoinsEvent', {
+    //   method: 'POST', // or 'PUT'
+    //   headers: {
+    //     'Content-Type': 'application/json',
+    //   },
+    //   body: JSON.stringify({eventId: eventID, username: username}),
+    // })
+    //   .then((response) => response.json())
+    //   .then((arrOfTasks) => setTasks(arrOfTasks))
+    //   .catch((err) => console.log('join event err ', err));
 
-    // writes event info to event Atom
-    fetch('/getUserEventInfo', {
-      method: 'GET', // or 'PUT'
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify(username),
-    })
-      .then((response) => response.json())
-      .then((eventInfo) => setEvent(eventInfo))
-      .catch((err) => console.log('join event err ', err));
-  };
+    // // // writes event info to event Atom
+    // fetch('/getUserEventInfo', {
+    //   method: 'POST', 
+    //   headers: {
+    //     'Content-Type': 'application/json',
+    //   },
+    //   body: JSON.stringify({username: username}),
+    // })
+    //   .then((response) => response.json())
+    //   .then((eventInfo) => setEvent(eventInfo))
+    //   .catch((err) => console.log('join event err ', err));
+
+      // setJoinedEvent(true)
+      
+  // };
+
+  // useEffect(() => {
+  //   if (joinedEvent) {
+  //     history.push('/app');
+  //   }
+  // }, [tasks, event]); 
+
+
   return (
     <div className="joinEventDiv">
       <h2>Join Event</h2>
       <form>
         <input type="text" name="joinEventId" placeholder="Event ID" id="eventID" />
-        <button type="submit" value="Join" onClick={handleSubmit} />
+        <button type="submit" value="Join"  >Join The Event</button>
       </form>
     </div>
+    
   );
 };
 
