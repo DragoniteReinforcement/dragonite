85ae6b7629dc49aef183ac4e9c89072e80449177
Patrick Sullivan <patrick@jsullivan.org>
2021-07-08 18:04:39 -0700

    Refactored serer and added member progress component

--- client/components/MembersProgress.jsx
+++ client/components/MembersProgress.jsx
@@ -1,9 +1,35 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { useRecoilState, useRecoilValue } from 'recoil';
+import atoms from '../atoms';
+// import ScoreItem from '../ScoreItem.jsx';
 
 const MembersProgress = () => (
+  // const [scores, setScores] = useRecoilState(atoms.scores);
+  // const userInfo = useRecoilValue(atoms.userInfo);
+  // // fetch request to /getLeaderboard which will return the scores object containing usernames and current scores
+  // useEffect(() => {
+  //   fetch('/getLeaderboard', {
+  //     method: 'POST',
+  //     headers: { 'Content-Type': 'application/json' },
+  //     body: JSON.stringify({ username: userInfo.username }),
+  //   })
+  //     .then((res) => res.json())
+  //     .then((data) => setScores(data))
+  //     .catch((err) => console.log('ERROR: ', err));
+  // }, [userInfo]);
+  // // create an array to hold each ScoreItem (containing username and score)
+  // const scoresList = [];
+  // for (const username in scores) {
+  //   scoresList.push(<ScoreItem username={username} score={scores[username]} />);
+  // }
+  // return (
+  //   <div className="progressDiv">
+  //     <h2>Members' Progress</h2>
+  //     <div>{scoresList}</div>
+  //   </div>
+  // );
   <div className="progressDiv">
     <h2>Members' Progress</h2>
   </div>
 );
-
 export default MembersProgress;
---
+++ client/components/scoreItem.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const ScoreItem = (props) => (
+  <div>
+    <span>
+      <p>{props.username}</p>
+      <p>{props.score}</p>
+    </span>
+  </div>
+);
+
+export default SchemaItem;
