b613e090e9f6cd16a023882d78c17a47ebf15a23
Patrick Sullivan <patrick@jsullivan.org>
2021-07-16 11:20:39 -0700

    Resolved db issue

--- server/models/appModel.js
+++ server/models/appModel.js
@@ -1,7 +1,7 @@
 const { Pool } = require('pg');
 
 const PG_URI =
-  'postgres://rkxawmbo:kn-5aipngU2ojtd4vShLnU_gCzzyydXw@otto.db.elephantsql.com/rkxawmbo'; // <here is where I have pasted local host
+  'postgres://zxbnhcgk:st_4SeSbTLcX5mpVSBgjr20bcBwsiLyr@otto.db.elephantsql.com/zxbnhcgk'; // <here is where I have pasted local host
 
 // create a new pool here using the connection string above
 const pool = new Pool({
